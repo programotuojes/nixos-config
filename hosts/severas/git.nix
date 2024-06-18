e6ef6cabc927c1861e84b45870a39c323c2a0cf9
Gustas Klevinskas <programotuojes@users.noreply.github.com>
2024-06-18 12:11:49 +0300

    Add ssh key

--- hosts/severas/git.nix
+++ hosts/severas/git.nix
@@ -10,6 +10,7 @@
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO+gwgHsuoQ7bWMYJQ+F5CZgjFvdBNZ3eEAlmZ9rmk3R gustas@LBook"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAkqnik4bo2t3eVaQtaAaPHLBjVv53nrUYf7B15W3B6u r5800x3d@DESKTOP-V85LMH7"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAYn8UTA0jhrwOezoVHJAgO6FLNvzuIrczAMumUkgSJh thecheesy0ne@sauliusOMEN"
     ];
   };
 
