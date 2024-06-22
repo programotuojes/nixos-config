35e37bd43e9c9c7e1d50842545d9adda050a3b20
Gustas Klevinskas <programotuojes@users.noreply.github.com>
2024-06-22 23:29:12 +0300

    Remove "Unknown-1" display

--- hosts/LBook/nvidia.nix
+++ hosts/LBook/nvidia.nix
@@ -1,21 +1,12 @@
 { ... }:
 
 {
-  services.xserver.dpi = 96;
-
-  # Fix glitches after resuming from sleep
   boot.kernelParams = [
-    # https://github.com/hyprwm/Hyprland/issues/2950#issuecomment-1684074408
-    # "NVreg_RegistryDwords=\"OverrideMaxPerf=0x1\""
-
-    # https://bugs.kde.org/show_bug.cgi?id=448866
-    # "NVreg_PreserveVideoMemoryAllocations=1"
-    # "NVreg_TemporaryFilePath=/var/tmp"
+    "nvidia_drm.fbdev=1"
   ];
 
   hardware.nvidia = {
     modesetting.enable = true;
-    powerManagement.enable = true; # Can't wake up in Wayland when true https://github.com/NixOS/nixpkgs/issues/254614
-    # package = config.boot.kernelPackages.nvidiaPackages.production;
+    powerManagement.enable = true;
   };
 }
