8f230eac8f306a95fd670877eab39afb354db505
Gustas Klevinskas <programotuojes@users.noreply.github.com>
2025-06-09 16:51:10 +0300

    minecraft: use systemd & disable server

--- hosts/severas/minecraft.nix
+++ hosts/severas/minecraft.nix
@@ -2,7 +2,7 @@
 
 {
   nixpkgs.overlays = [ inputs.nix-minecraft.overlay ];
-  environment.defaultPackages = [ pkgs.tmux ];
+
   users.groups.${config.services.minecraft-servers.group}.members = [
     "gustas"
   ];
@@ -11,8 +11,10 @@
     enable = true;
     eula = true;
     openFirewall = true;
+    managementSystem.systemd-socket.enable = true;
+
     servers.klevas = {
-      enable = true;
+      enable = false;
       enableReload = true;
       package = pkgs.paperServers.paper-1_21_5;
       jvmOpts = "-Xms6G -Xmx6G -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true";
