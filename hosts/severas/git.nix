f62bb68b3d70e0cf48ef3e7a437e27faf08331a5
Gustas Klevinskas <programotuojes@users.noreply.github.com>
2025-10-15 08:20:22 +0300

    Remove unused SSH keys from git

--- hosts/severas/git.nix
+++ hosts/severas/git.nix
@@ -7,15 +7,7 @@
     home = "/pool/git";
     createHome = true;
     shell = "${pkgs.git}/bin/git-shell";
-    openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO+gwgHsuoQ7bWMYJQ+F5CZgjFvdBNZ3eEAlmZ9rmk3R gustas@LBook"
-      # Vilius
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAkqnik4bo2t3eVaQtaAaPHLBjVv53nrUYf7B15W3B6u r5800x3d@DESKTOP-V85LMH7"
-      # Saulius
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMcDzSYVzocEOCpUiDIagqXVHcM1/eisUdGT9PPw1Ke0 thecheesy0ne@sauliusOMEN"
-      # Pijus
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIINDxCOg2z/PD9dMOXd5Leos+jDK48c5I7bwAIXISXju" 
-    ];
+    openssh.authorizedKeys.keys = [];
   };
 
   users.groups.git = {};
